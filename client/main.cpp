8266a5442d70561b3ca26af344750d6370b1af6d
Michele Crepaldi <43987989+cmikke97@users.noreply.github.com>
2020-07-25 13:14:14 +0200

    client main created

---
+++ client/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by michele on 25/07/2020.
+//
+
+/**
+ * the client main function
+ *
+ * @param argc command argument number
+ * @param argv command arguments
+ * @return exit value
+ * @author Michele Crepaldi s269551
+ */
+int main(int argc, char **argv) {
+
+}
