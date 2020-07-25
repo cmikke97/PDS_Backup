bc0ec6e90fba0d49329bbc8d1edba3be40194352
michele <s234497@studenti.polito.it>
2020-07-25 13:09:19 +0200

    client main created

--- client/main.cpp
+++ client/main.cpp
@@ -2,3 +2,14 @@
 // Created by michele on 25/07/2020.
 //
 
+/**
+ * the client main function
+ *
+ * @param argc command argument number
+ * @param argv command arguments
+ * @return exit value
+ * @author Michele Crepaldi s269551
+ */
+int main(int argc, char **argv) {
+    
+}
